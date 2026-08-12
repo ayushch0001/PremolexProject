4bbe955308aea798635f51ded0c0509e67646508
ayushch0001 <omrudra998gmail.com>
2026-08-12 17:33:27 +0530

    all changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.premolex.com/api',
+  apiUrl: 'https://premolex.com/api',
   firebaseConfig: {
-    apiKey: 'AIzaSyCVXND9DdDhfdm81tKR5hNTC6Z1-w_0NA0',
-    authDomain: 'premolex-a3c1c.firebaseapp.com',
-    projectId: 'premolex-a3c1c',
-    storageBucket: 'premolex-a3c1c.firebasestorage.app',
-    messagingSenderId: '610846055734',
-    appId: '1:610846055734:web:46db90142233e4b02da797',
+    apiKey: "AIzaSyCy98A5SOHY2JF0NiPWONTLE_29YVMVV3Y",
+    authDomain: "prem-industries-78cf9.firebaseapp.com",
+    projectId: "prem-industries-78cf9",
+    storageBucket: "prem-industries-78cf9.firebasestorage.app",
+    messagingSenderId: "595204548626",
+    appId: "1:595204548626:web:887d4d51c31c76040cd0cb",
     measurementId: 'G-G3CZXJM65L',
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,15 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
   firebaseConfig: {
-    apiKey: 'AIzaSyCVXND9DdDhfdm81tKR5hNTC6Z1-w_0NA0',
-    authDomain: 'premolex-a3c1c.firebaseapp.com',
-    projectId: 'premolex-a3c1c',
-    storageBucket: 'premolex-a3c1c.firebasestorage.app',
-    messagingSenderId: '610846055734',
-    appId: '1:610846055734:web:46db90142233e4b02da797',
+    apiKey: "AIzaSyCy98A5SOHY2JF0NiPWONTLE_29YVMVV3Y",
+    authDomain: "prem-industries-78cf9.firebaseapp.com",
+    projectId: "prem-industries-78cf9",
+    storageBucket: "prem-industries-78cf9.firebasestorage.app",
+    messagingSenderId: "595204548626",
+    appId: "1:595204548626:web:887d4d51c31c76040cd0cb",
     measurementId: 'G-G3CZXJM65L',
   },
+
 };
+
+
